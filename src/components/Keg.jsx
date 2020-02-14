7aca78d66f6fb39fcdd3d5a733e76c0002d511ad
LiamPKenna <liampkenna@gmail.com>
2020-02-14 12:04:23 -0800

    Add alcohol content to Keg

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -16,10 +16,26 @@ function Keg(props) {
     borderRadius: '5px',
     margin: '10px'
   };
+  const kegTopStyle = {
+    display: 'grid',
+    gridTemplateColumns: '1fr 90px'
+  };
+  const alcoholStyle = {
+    textAlign: 'right',
+    paddingRight: '20px'
+  };
   return (
     <div style={kegStyle}>
-      <h3>{props.keg.name}</h3>
-      <h4>{props.keg.brand}</h4>
+      <div style={kegTopStyle}>
+        <div>
+          <h3>{props.keg.name}</h3>
+          <h4>{props.keg.brand}</h4>
+        </div>
+        <div style={alcoholStyle}>
+          <p>Alcohol:</p>
+          <p>{props.keg.alcoholContent}%</p>
+        </div>
+      </div>
       <Counter pints={props.keg.pints} employee={props.employee} />
       <Price price={props.keg.price} />
     </div>
