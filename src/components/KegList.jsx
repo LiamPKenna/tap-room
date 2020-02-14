31432c5628cc44fb67f60d0fd49b639e3172caba
LiamPKenna <liampkenna@gmail.com>
2020-02-14 11:02:05 -0800

    Add additional style to KegList

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -6,14 +6,16 @@ function KegList(props) {
   const listStyle = {
     margin: '20px',
     display: 'flex',
-    flexFlow: 'row wrap'
+    flexFlow: 'row wrap',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    justifyContent: 'center'
   };
   return (
     <div style={listStyle}>
       {kegs.map((keg,index) => 
         (<Keg keg={keg} key={index} employee={props.employee} />)
       )}
-      <h1>Hello</h1>
     </div>
   )
 }
