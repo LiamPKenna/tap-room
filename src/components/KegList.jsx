e21b90fc226fadbb1876dab94b92e55a221716b7
LiamPKenna <liampkenna@gmail.com>
2020-02-14 09:34:29 -0800

    Add Kegs to KegList

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Keg from './Keg';
+import kegs from '../kegs';
 
 function KegList() {
   return (
+    <div>
+
+    {kegs.map((keg,index) => 
+      (<Keg keg={keg} key={index} />)
+    )}
     <h1>Hello</h1>
+    </div>
   )
 }
 
