8534dcd8c67a9a348929748aa6b840b098016637
LiamPKenna <liampkenna@gmail.com>
2020-02-14 10:32:17 -0800

    Add initial styles to KegList

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -3,8 +3,13 @@ import Keg from './Keg';
 import kegs from '../kegs';
 
 function KegList(props) {
+  const listStyle = {
+    margin: '20px',
+    display: 'flex',
+    flexFlow: 'row wrap'
+  };
   return (
-    <div>
+    <div style={listStyle}>
       {kegs.map((keg,index) => 
         (<Keg keg={keg} key={index} employee={props.employee} />)
       )}
