2e360dfe34eb41f9fa3a503a36750072ad10ebcd
LiamPKenna <liampkenna@gmail.com>
2020-02-14 08:53:42 -0800

    Add static keg data

---
+++ src/kegs.js
@@ -0,0 +1,25 @@
+const kegs = [
+  {
+    name: 'Fred',
+    brand: 'Hair of the Dog',
+    price: 8,
+    alcoholContent: 10,
+    pints: 124
+  },
+  {
+    name: 'Warsteiner Premium Verum',
+    brand: 'Warsteiner',
+    price: 4.5,
+    alcoholContent: 4.8,
+    pints: 80
+  },
+  {
+    name: 'YSB',
+    brand: 'Rogue Ales',
+    price: 5,
+    alcoholContent: 5.5,
+    pints: 5
+  },
+];
+
+export default kegs;
