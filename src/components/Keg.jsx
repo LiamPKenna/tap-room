23e7a44c955e9696a55f3a5454534e703e53e9f8
LiamPKenna <liampkenna@gmail.com>
2020-02-14 10:52:33 -0800

    Add style to Keg component

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -3,8 +3,15 @@ import Counter from './Counter';
 import Price from './Price';
 
 function Keg(props) {
+  const kegStyle = {
+    width: '40%',
+    border: '1px solid darkgrey',
+    borderRadius: '5px',
+    margin: '10px',
+    padding: '20px'
+  };
   return (
-    <div>
+    <div style={kegStyle}>
       <h3>{props.keg.name}</h3>
       <h4>{props.keg.brand}</h4>
       <Counter pints={props.keg.pints} employee={props.employee} />
