b2a08405178ac85e77daa9ea489ddaccc928672d
LiamPKenna <liampkenna@gmail.com>
2020-02-14 11:13:04 -0800

    Add display of price with dynamic background color

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
-function Price(params) {
+function Price(props) {
+  const makeMoney = (value) => {
+    return Number.parseFloat(value).toFixed(2);
+  }
+  const colorCode = (value) => {
+    if (value > 7.5) {
+      return 'red';
+    } else if (value >= 5) {
+      return 'yellow';
+    } else {
+      return 'green';
+    }
+  }
+  const priceStyles = {
+    backgroundColor: colorCode(props.price),
+
+  }
   return (
-    <p>Price</p>
+    <div style={priceStyles}>
+      {`$${makeMoney(props.price)}`}
+    </div>
   );
 }
 
