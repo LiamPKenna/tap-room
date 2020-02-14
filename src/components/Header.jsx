e16a0b679d7a74f7037f52aed06ae90fe15599a7
LiamPKenna <liampkenna@gmail.com>
2020-02-14 10:04:33 -0800

    Add Header styles and remove body margin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,16 @@ function Header() {
     textAlign: 'center',
     color: '#337ab7',
     textDecoration: 'none',
-    paddingTop: '15px'
+    paddingTop: '30px'
+  };
+  const headerGrid = {
+    display: 'grid',
+    gridTemplateColumns: '1fr 200px',
+    backgroundColor: 'grey',
+    padding: '10px'
   };
   return (
-    <div>
+    <div style={headerGrid}>
       <h1>Top Tap Room</h1>
       <div style={linkDiv}>
         <Link to="/">PATRON</Link> | <Link to="/emp">EMPLOYEE</Link>
