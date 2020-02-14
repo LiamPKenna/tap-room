210cc890ea1e8630b80d3f2dd260ef4170e5cd06
LiamPKenna <liampkenna@gmail.com>
2020-02-14 11:33:45 -0800

    Change keg border in employee view when low

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -3,12 +3,18 @@ import Counter from './Counter';
 import Price from './Price';
 
 function Keg(props) {
+  const checkIfLow = (employee, number) => {
+    if (employee && number < 10) {
+      return '3px solid red';
+    } else {
+      return '1px solid darkgrey';
+    }
+  }
   const kegStyle = {
     width: '40%',
-    border: '1px solid darkgrey',
+    border: checkIfLow(props.employee, props.keg.pints),
     borderRadius: '5px',
-    margin: '10px',
-    padding: '20px'
+    margin: '10px'
   };
   return (
     <div style={kegStyle}>
