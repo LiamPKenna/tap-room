dccb4448b953be3d244740eb4052cd60f580f12a
LiamPKenna <liampkenna@gmail.com>
2020-02-14 09:34:05 -0800

    Add Keg component basic structure

---
+++ src/components/Keg.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Counter from './Counter';
+import Price from './Price';
+
+function Keg(props) {
+  return (
+    <div>
+      <h3>{props.keg.name}</h3>
+      <h4>{props.keg.brand}</h4>
+      {/* <Counter pints={props.keg.pints} />
+      <Price price={props.keg.price} /> */}
+    </div>
+  );
+
+}
+
+export default Keg;
