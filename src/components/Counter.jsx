0bffa0cf4f541e1a5eaa747abb8a9abff1839445
LiamPKenna <liampkenna@gmail.com>
2020-02-14 11:01:34 -0800

    Add sell button if user is employee

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 function Counter(props) {
   console.log(props);
   
   const checkEmp = () => {    
     if (props.employee) {
-      return (<h5>EMPLOYEE!</h5>)
-    } else {
-      return (<h6>Patron</h6>)
-    }
+      return (<Button variant="contained">SELL</Button>)
+    } 
   }
   return (
     <div>
