699d5c2d5fd1a3f2aac7502035c71496b9520d2c
LiamPKenna <liampkenna@gmail.com>
2020-02-21 08:37:35 -0800

    Add hooks to the rest of the inputs

--- src/components/NewKeg.jsx
+++ src/components/NewKeg.jsx
@@ -7,6 +7,9 @@ import Button from '@material-ui/core/Button';
 function NewKeg() {
 
   const [name, setName] = useState('');  
+  const [brand, setBrand] = useState('');  
+  const [abv, setAbv] = useState('');  
+  const [price, setPrice] = useState('');  
 
   const formStyle = {
     margin: '40px auto',
@@ -15,30 +18,58 @@ function NewKeg() {
     borderRadius: '10px',
     width: '80%',
   };
+
+  const constructKeg = (e) => {
+    e.preventDefault()
+    const newKeg = {
+      name: name,
+      brand: brand,
+      alcoholContent: abv,
+      price: price
+    };
+    setName('');
+    setBrand('');
+    setAbv('');
+    setPrice('');
+    console.log(newKeg);  
+  }
   return (
     <div style={formStyle}>
-      <form action="">
+      <form onSubmit={constructKeg}>
         <FormControl fullWidth>
           <InputLabel htmlFor="name">Beer Name</InputLabel>
           <Input 
             id="name" 
             value={name} 
-            onChange={(e) => setName(e.target.value) } />
+            onChange={(e) => setName(e.target.value) }
+            required />
           <br/>
         </FormControl>
         <FormControl fullWidth>
           <InputLabel htmlFor="brand">Brand</InputLabel>
-          <Input id="brand" />
+          <Input 
+            id="brand"
+            value={brand} 
+            onChange={(e) => setBrand(e.target.value) }
+            required />
           <br/>
         </FormControl>
         <FormControl fullWidth>
           <InputLabel htmlFor="abv">ABV</InputLabel>
-          <Input id="abv" />
+          <Input 
+            id="abv"
+            value={abv} 
+            onChange={(e) => setAbv(e.target.value) }
+            required />
           <br/>
         </FormControl>
         <FormControl fullWidth>
           <InputLabel htmlFor="price">Beer Price</InputLabel>
-          <Input id="price" />
+          <Input 
+            id="price"
+            value={price} 
+            onChange={(e) => setPrice(e.target.value) }
+            required />
           <br/>
         </FormControl>
         <Button variant="contained" type="submit" fullWidth>SUBMIT</Button>
