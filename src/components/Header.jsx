1163e5e27286edbb97a51ec0c25b095ee6449d25
LiamPKenna <liampkenna@gmail.com>
2020-02-21 14:49:04 -0800

    Add style to header h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,9 +14,18 @@ function Header() {
     backgroundColor: '#d1d1d1',
     padding: '10px'
   };
+  const headerStyle = {
+    fontSize: '72px',
+    background: 'linear-gradient(0deg, rgba(75,140,92,1) 29%, rgba(154,103,83,1) 54%, rgba(165,172,96,1) 71%)',
+    webkitBackgroundClip: 'text',
+    webkitTextFillColor: 'rgba(0, 0, 0, .35)',
+    marginLeft: '30px',
+    textShadow: '4px 4px 4px rgba(255, 255, 255, .5)',
+    fontStyle: 'italic'
+  };
   return (
     <div style={headerGrid}>
-      <h1>Tip Top Tap Room</h1>
+      <h1 style={headerStyle} >Tip Top Tap Room</h1>
       <div style={linkDiv}>
         <Link to="/">PATRON</Link> | <Link to="/emp">EMPLOYEE</Link>
       </div>
