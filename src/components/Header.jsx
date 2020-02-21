3d00100aa2f4e4492fe5e5e31f0e036330f6d891
LiamPKenna <liampkenna@gmail.com>
2020-02-21 15:36:09 -0800

    Refactor to improve mobile display

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,31 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
-  const linkDiv = {
+  const linkDivStyle = {
     textAlign: 'center',
     color: '#337ab7',
-    textDecoration: 'none',
-    paddingTop: '30px'
-  };
-  const headerGrid = {
-    display: 'grid',
-    gridTemplateColumns: '1fr 200px',
-    backgroundColor: '#d1d1d1',
-    padding: '10px'
+    textDecoration: 'none'
   };
   const headerStyle = {
-    fontSize: '72px',
+    fontSize: '65px',
     background: 'linear-gradient(0deg, rgba(75,140,92,1) 29%, rgba(154,103,83,1) 54%, rgba(165,172,96,1) 71%)',
-    webkitBackgroundClip: 'text',
-    webkitTextFillColor: 'rgba(0, 0, 0, .35)',
-    marginLeft: '30px',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'rgba(0, 0, 0, .35)',
+    margin: '20px auto',
     textShadow: '4px 4px 4px rgba(255, 255, 255, .5)',
     fontStyle: 'italic'
   };
+  const headerDiv = {
+    backgroundColor: '#d1d1d1',
+    padding: '10px',
+    textAlign: 'center'
+  };
   return (
-    <div style={headerGrid}>
+    <div style={headerDiv}>
       <h1 style={headerStyle} >Tip Top Tap Room</h1>
-      <div style={linkDiv}>
+      <div style={linkDivStyle}>
         <Link to="/">PATRON</Link> | <Link to="/emp">EMPLOYEE</Link>
       </div>
     </div>
