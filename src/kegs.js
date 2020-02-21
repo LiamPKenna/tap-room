755dec6cc4877dd26b60031d0e5b103543a7bddd
LiamPKenna <liampkenna@gmail.com>
2020-02-21 09:55:56 -0800

    Add id to keg data

--- src/kegs.js
+++ src/kegs.js
@@ -4,35 +4,40 @@ const kegs = [
     brand: 'Hair of the Dog',
     price: 8,
     alcoholContent: 10,
-    pints: 124
+    pints: 124,
+    id: 1
   },
   {
     name: 'Warsteiner Premium Verum',
     brand: 'Warsteiner',
     price: 4,
     alcoholContent: 4.8,
-    pints: 80
+    pints: 80,
+    id: 2
   },
   {
     name: 'YSB',
     brand: 'Rogue Ales',
     price: 5.5,
     alcoholContent: 5.5,
-    pints: 5
+    pints: 5,
+    id: 3
   },
   {
     name: 'Pliny the Elder',
     brand: 'Russian River',
     price: 6.5,
     alcoholContent: 8,
-    pints: 30
+    pints: 30,
+    id: 4
   },
   {
     name: 'Hefeweizen',
     brand: 'Widmer',
     price: 5,
     alcoholContent: 6.2,
-    pints: 105
+    pints: 105,
+    id: 5
   },
 ];
 
