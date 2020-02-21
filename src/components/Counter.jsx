f5ba7b497be826b3d64aa46353f2fde77574a91e
LiamPKenna <liampkenna@gmail.com>
2020-02-21 12:11:18 -0800

    Add dynamic background based on pints left

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import PropTypes from "prop-types";
 
 function Counter(props) {
+
+  const [percentLeft, setPercentLeft] = useState((props.pints*100)/124);
+  const [bgColor, setBgColor] = useState(`linear-gradient(0deg, rgba(0,0,0,1) ${percentLeft}%, rgba(54,54,54,1) ${percentLeft + 6}%, rgba(54,54,54,1) 100%)`);
   
   const checkEmp = () => {    
     if (props.employee) {
@@ -15,13 +18,17 @@ function Counter(props) {
     } 
   }
 
-  const sell = () => { props.sellPint(props.id) };
+  const sell = () => { 
+    props.sellPint(props.id);
+    setPercentLeft((props.pints*100)/124);
+    setBgColor(`linear-gradient(0deg, rgba(0,0,0,1) ${percentLeft}%, rgba(54,54,54,1) ${percentLeft + 6}%, rgba(54,54,54,1) 100%)`);
+  }
 
   const counterStyle = {
     display: 'grid',
     gridTemplateColumns: (props.employee)? '1fr 1fr' : '1fr',
     textAlign: 'center',
-    backgroundColor: '#363636',
+    background: bgColor,
     color: 'white',
     border: '2px solid #d1d1d1'
   };
@@ -31,8 +38,7 @@ function Counter(props) {
     fontSize: '18px'
   };
   const btnStyle = {
-    margin: '10px',
-    backgroundColor: (props.pints > 10) ? 'default' : 'red'
+    margin: '10px'
   }
   return (
     <div style={counterStyle}>
